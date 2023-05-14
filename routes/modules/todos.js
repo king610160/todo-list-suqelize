b90cdb4c529f78f1fa4f8c92edbdff4169da06c6
king610160 <king610160@gmail.com>
2023-05-14 13:35:39 +0800

    feat: modified todos.js in route,  add edit.hbs, modified main.hbs.

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -19,7 +19,11 @@ router.get('/:id', (req, res) => {
 })
 
 router.get('/:id/edit', (req, res) => {
-  
+    // const userId = req.user._id
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .then((todo) => res.render('edit', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
 })
 
 router.post('/', (req, res) => {
@@ -27,11 +31,28 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  
+//   const userId = req.user._id
+  const id = req.params.id
+  const { name, isDone } = req.body   //使用者輸入的表單內容,拿取對應參數
+
+  return Todo.findByPk(id) //呼叫Todo，用id找到對應資料(使用者改id頁面的id去找)
+    .then(todo => { //不使用lean是因為還不需要解析資料
+      todo.name = name //資料庫裡的name變成使用者輸入的name
+      todo.isDone = (isDone === 'on')
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))  //新增完成後導回詳細頁，確認輸入資料
+    .catch(error => console.log(error))
 })
 
 router.delete('/:id', (req, res) => {
- 
+    // const userId = req.user._id
+    const id = req.params.id
+    return Todo.findByPk(id)
+    //   .then(() => console.log(Todo.findByPk(id)))
+      .then((todo) => todo.remove())  //找到對應id才能做remove
+      .then(() => res.redirect('/'))  //remove後到根目錄
+      .catch(error => console.log(error))
 })
 
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-
+const bcrypt = require('bcryptjs')  // 載入套件
 const db = require('../../models')
-
-const Todo = db.Todo
 const User = db.User
+
+
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -20,8 +20,27 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 router.get('/logout', (req, res) => {
